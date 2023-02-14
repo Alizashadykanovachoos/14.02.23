99c3352eac78c999da69febf6d83379750d0c08a
Aliza <erjan984237@gmail.com>
2023-02-14 15:09:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function sum(a,b,c){
+  console.log(a + b + c)
+
+} 
+sum(4, 5 ,6);
+
+function rectangleArea(x1, x2){
+  console.log(x1 * x2);
+  }
+  rectangleArea(2,4);
+  rectangleArea(5,6)
+
+  for (let a = 1; a <=10; a++){
+    console.log(a)
+  }
+   
+  let example1 ={};// обьекты
+  let Joldosh = {
+  name: "Joldosh", 
+  age: 22,
+  isEmployed:false
+};
+
+
+
+ // Viev, edit,add
+ console.log(Joldosh.name)
+ Joldosh.age = 23;
+ console.log(Joldosh.age)
+ Joldosh.favouriteBoooks = ["Harry Potter", "Little Women"];
+ console.log(Joldosh.favouriteBoooks);
+
+ 
+
+
+
+
+  
+  
