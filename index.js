c7813d23b8c56ee123e947c86d39e51f4cedfd4e
Aliza <erjan984237@gmail.com>
2023-02-20 22:25:26 +0600

    added nrw file

--- index.js
+++ index.js
@@ -16,10 +16,23 @@ function rectangleArea(x1, x2){
    
   let example1 ={};// обьекты
   let Joldosh = {
-  name: "Joldosh", 
-  age: 22,
-  isEmployed:false
-};
+    name: "Joldosh", 
+    age: 22,
+    isEmployed:false,
+    lastname: "ali",
+    sayHello: function() {
+      console.log( "Hello " +  " I'm "  + this.name);
+      
+    },
+    sayAge: function(){
+      console.log("i'm "+ this.age + " years old")
+    },
+    sayBye:function() {
+      console.log("Bye");
+    },
+    
+  };
+
 
 
 
@@ -31,7 +44,57 @@ function rectangleArea(x1, x2){
  console.log(Joldosh.favouriteBoooks);
 
  //delete
- 
+   delete Joldosh.favouriteBoooks;//delete
+
+   Joldosh.sayHello();
+   Joldosh.name ="Jol";
+   Joldosh.sayHello();
+   Joldosh.sayAge();
+  
+
+
+   let example = {
+    number:0,
+    inc: function(){
+      this.number = this.number + 1;
+    },
+    dcr: function(){
+      this.number = this.number + 1;
+    },
+    dcr: function(num){
+      this.number = this.number -1
+    },
+    add: function(num){
+      this.number = this.number + num;
+    },
+    sabtract: function(num){
+      this.number = this.number - num;
+    }, 
+    remowe: function(num){
+      this.number = this.number + num;
+    },
+    print: function(){
+      console.log(this.number);
+    }
+   }
+   console.log(example.number);
+   example.inc();
+   example.inc();
+   console.log(example.number);
+   console.log(example.number);
+   example.inc();
+   example.inc();
+   console.log(example.number);
+   example.add(5);
+   console.log(example.number);
+   example.remowe(3);
+   example.print(); 
+   
+
+
+
+
+   
 
 
 
